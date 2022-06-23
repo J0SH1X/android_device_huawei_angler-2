45f628d1d0fb9f1f4c846f1efd819eaac1a2ef7d
Derek <nielson925@gmail.com>
2022-06-22 22:46:29 -0600

    angler: Build radio config 1.0
    Change-Id: I53dcf14333779718570eacc69d98a322a2af971b

--- device.mk
+++ device.mk
@@ -261,7 +261,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.0 \
-    android.hardware.radio.deprecated@1.0
+    android.hardware.radio.deprecated@1.0 \
+    android.hardware.radio.config@1.0-service
 
 # SEC config
 PRODUCT_COPY_FILES += \
