a94227b71db57c2e1a84e6244915d2ad60eed40a
Derek <nielson925@gmail.com>
2024-07-08 22:11:10 +0000

    angler: uprev audio hal to 6.0

--- hidl.mk
+++ hidl.mk
@@ -16,10 +16,10 @@
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.soundtrigger@2.0-impl
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio@6.0-service \
+    android.hardware.audio.effect@6.0-impl \
+    android.hardware.soundtrigger@6.0-impl
 
 # Bluetooth
 PRODUCT_PACKAGES += \
