d75ac46b0a7623298a482bcd41b63554f1b403f2
J0SH1X <aljoshua.hell@gmail.com>
2025-03-01 04:16:19 +0100

    device: reenable camera packages

--- device.mk
+++ device.mk
@@ -20,12 +20,12 @@
 # Everything in this directory will become public
 
 # # Camera
-# PRODUCT_PACKAGES += \
-#     camera.msm8994 \
-#     libmmcamera_interface \
-#     libmmjpeg_interface \
-#     libqomx_core \
-#     mm-qcamera-app
+PRODUCT_PACKAGES += \
+    camera.msm8994 \
+    libmmcamera_interface \
+    libmmjpeg_interface \
+    libqomx_core \
+    mm-qcamera-app
 
 
 # Characteristics
