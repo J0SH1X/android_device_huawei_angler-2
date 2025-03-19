3f9f14d8e6e15b62488db146ba0b3cc6977055e5
J0SH1X <aljoshua.hell@gmail.com>
2025-03-19 16:09:16 +0000

    Angler: add libshim_awb
    the camera stack is 32bit only but yet somehow a 64bit lib exists which tries to use theese symbols which are only present in 32bit.
    Add a shim with void funcs to make elfchecks happy nothing is ever gonna try to use this anyway

---
+++ libshims/libshim_awb/awb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void aec_extreme_color_update() {}
+void awb_bayer_check_stat_for_outdoor_exposure_heuristic_for_subzones() {}
+void awb_bayer_check_stat_for_single_color_zone() {}
+void awb_bayer_exposure_outdoor_heuristic_with_subdecision_zones() {}
+void awb_bayer_outlier_compute_ratio_mlc_zones() {}
+void awb_bayer_outlier_init_cal_offset_mlc() {}
+void awb_bayer_outlier_init_single_color_zone() {}
+void awb_bayer_outlier_mlc_zone_with_offset() {}
+void awb_bayer_outlier_process() {}
+void awb_bayer_subzones_search_A_H_line() {}
+void awb_bayer_subzones_search_F_A_line() {}
+void awb_bayer_subzones_search_day_flo_line() {}
+void awb_bayer_subzones_search_dayline1() {}
+void awb_bayer_subzones_search_dayline2() {}
+void awb_bayer_subzones_search_dayline3() {}
+void awb_bayer_subzones_search_flo_line() {}
+void awb_single_color_heuristic() {}
